24d395c99d6b859ff3538e1a64fce33d18504751
Muralidhar Kushalraj <muralikushalraj@outlook.com>
2024-06-24 21:23:04 +0530

    Search Controller update

---
+++ Src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItems([FromQuery] SearchParam searchParam)
+        {
+            var query = DB.PagedSearch<Item, Item>();
+            query.Sort(x => x.Ascending(y => y.Make));
+            if (!string.IsNullOrEmpty(searchParam.SearchTerm))
+                query.Match(Search.Full, searchParam.SearchTerm).SortByTextScore();
+            query = searchParam.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(y => y.Make)),
+                "new" => query.Sort(x => x.Descending(y => y.CreatedAt)),
+                _ => query.Sort(x => x.Ascending(y => y.AuctionEnd))
+            };
+            query = searchParam.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+            };
+            if (!string.IsNullOrEmpty(searchParam.Seller))
+                query.Match(x => x.Seller == searchParam.Seller);
+            if (!string.IsNullOrEmpty(searchParam.Winner))
+                query.Match(x => x.Winner == searchParam.Winner);
+            query.PageNumber(searchParam.PageNumber);
+            query.PageSize(searchParam.PageSize);
+            var result = await query.ExecuteAsync();
+            return Ok(new
+            {
+                results = result.Results,
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount
+            });
+        }
+    }
+}
--- Src/SearchService/Program.cs
+++ Src/SearchService/Program.cs
@@ -1,3 +1,8 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Data;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,5 +20,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+	await DbInitializer.InitDb(app);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex);
+}
 
 app.Run();
