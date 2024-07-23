aef6c2d19d87cba07c4f87e5867f56f8da055785
Muralidhar Kushalraj <muralikushalraj@outlook.com>
2024-07-23 13:31:51 +0530

    End of Section 7

--- Src/SearchService/Program.cs
+++ Src/SearchService/Program.cs
@@ -28,12 +28,11 @@ builder.Services.AddMassTransit(x =>
         //    r.Interval(5, TimeSpan.FromSeconds(10));
         //});
 
-        //cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
-        //{
-        //    host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-        //    host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
-        //});
-
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         cfg.ReceiveEndpoint("search-auction-created", e =>
         {
             e.UseMessageRetry(r => r.Interval(5, 5));
