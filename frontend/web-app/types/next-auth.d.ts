a11517d15fd4fdffe3edc2b84d5f43f5831490dc
Muralidhar Kushalraj <muralikushalraj@outlook.com>
2024-08-04 07:42:04 +0530

    End of section 10

--- frontend/web-app/types/next-auth.d.ts
+++ frontend/web-app/types/next-auth.d.ts
@@ -1,20 +1,24 @@
-import {DefaultSession} from 'next-auth';
+import { DefaultSession } from "next-auth";
 
-declare module 'next-auth' {
-    interface Session {
-        user: {
-            username: string
-        } & DefaultSession['user']
-    }
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      username: string;
+    } & DefaultSession["user"];
+  }
 
-    interface Profile {
-        username: string
-    }
+  interface Profile {
+    username: string;
+  }
+  interface User {
+    username: string;
+  }
 }
 
-declare module 'next-auth/jwt' {
-    interface JWT {
-        username: string
-        access_token?: string
-    }
+declare module "next-auth/jwt" {
+  interface JWT {
+    username: string;
+    access_token?: string;
+  }
 }
